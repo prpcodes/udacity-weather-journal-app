1a8239558b6741fce03504a5597f5ae7aa4649b7
prpwien <office@prp.wien>
2020-06-08 08:10:25 +0200

    Setup
    Setup server for developing

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
-
+const port = 3000;
 // Require Express to run server and routes
-
+var express = require('express')
 // Start up an instance of app
+var app = express()
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const server = app.listen(port, ()=>{console.log(`running on localhost: ${port}`)})
