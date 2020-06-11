356b0e2c7530a376d3216022d9f20964ae889d09
Philipp P <office@prp.wien>
2020-06-11 14:03:26 +0200

    added async Function to get JSON response from API

--- website/app.js
+++ website/app.js
@@ -1,9 +1,11 @@
 /* Global Variables */
-let baseURL = 'api.openweathermap.org/data/2.5/weather?zip=';
+let baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
 let zipCode = '10001';
-let apiKey = 'ef4e3b78548a4d6a518f96e68460622f';
+const apiPrefix = '&appid='
+const apiKey = `${apiPrefix}ef4e3b78548a4d6a518f96e68460622f`;
 
-//TODO: Add Country Code??
+//TODO: Figure out how to add Country Code??
+
 // API CALL = api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={your api key}
 console.log(`${baseURL}${zipCode}&appid=${apiKey}`);
 
@@ -14,29 +16,26 @@ console.log(newDate);
 
 
 
-document.getElementById('generate').addEventListener('click', getEntrys);
+document.getElementById('generate').addEventListener('click', performAction);
 
-function getEntrys(){
-    const text =  document.getElementById('feelings').value;
-    console.log(text)
-    const zip =  document.getElementById('zip').value;
-    console.log(zip)
-    //add response
-};
+const text =  document.getElementById('feelings').value;
 
-
-/*
-const getProjectData = async (baseURL, animal, key)=>{
-
-  const res = await fetch(baseURL+animal+key)
-  try {
-
-    const data = await res.json();
-    console.log(data)
-    return data;
-  }  catch(error) {
-    console.log("error", error);
-    // appropriately handle the error
-  }
-}
-*/
+function performAction(e){
+    const zipCode =  document.getElementById('zip').value;
+    getEntrys(baseURL,zipCode,apiKey)
+    console.log(getEntrys);
+    
+    }
+    const getEntrys = async (baseURL,zipCode,apiKey)=>{
+    
+      const res = await fetch(baseURL+zipCode+apiKey)
+      try {
+    
+        const data = await res.json();
+        console.log(data)
+        return data;
+      }  catch(error) {
+        console.log("error", error);
+        // appropriately handle the error
+      }
+    }
